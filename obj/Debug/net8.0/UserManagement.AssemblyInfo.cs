337bf17fd2913f3688a11bb2d8b37b0ccda446d6
Siam183 <qureshisiam816@gmail.com>
2026-06-16 21:04:08 +0600

    Final Release: Implemented UserStatusMiddleware and corrected layout syntax

--- obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef2569ea503ea1bf0c093c9b5f869ce23cc0bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
